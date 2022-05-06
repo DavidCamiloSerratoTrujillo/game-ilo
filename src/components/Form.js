e1a1716cf31795b8ccb4aea45f417ed2a65c020b
camilo <dcserratot@correo.udistrital.edu.co>
2022-05-05 23:44:35 -0500

    Arreglar logos

--- src/components/Form.js
+++ src/components/Form.js
@@ -36,7 +36,7 @@ const handleChangeSelectP = ({target}) =>{
           aria-describedby="search-addon"
         />
         <button type="submit" className="btn btn-outline-primary  rounded">
-          <img src="https://lh3.googleusercontent.com/JnwVneQZOi47RNPys9ukLOyYQl9pJAoJvOeifWfVQc3on5hmjbh0bJjUH5-Y2yUKmsnMCtV15sdGfPtRnL52s2KTSiuKIcAGA9_YL-ptq7kT-RZkfp2L1CkRr4FzmPx4t5fuusR1pSCT8pE4-vg9Outt558FQuuZzfDN0jW_Gc-2iP5uGldVtSq-7uvwQtzreFr_34cXmUSFb3kt3HRy8tiZjdCAzCgdgBEHF8hll0pQT5sDAuQBFSM-UPdvyaTRoVxpJ2JZr0QX7HK-KL61Cq1oehvb9Lt7MMAmTwq1r7OpimQCB5PNDnQYk7UZ4bWjh11hXh3d-Nmioy7o8MvSzN4BiWB51xTpjZapQKdKmdxKnYot6yXGCRZ1v572HNCTTtjPMhO7rX-Nfz8UQWUMMtkobPpi-8wgVYebefRd3Cg1piv06sJahnx0Ugzs350yPy9Hf6CgQBIlaVKdtH0v4wJpK_BLitTsIYqTbjShMPp6T4At1H0zon9zw3H9DOipOYdguZUr3ap4h-i3XEZscloO6z0Pz39vrSANk98yyDMXS_zmUC_YfrW2eoUs5GTxbF-ao_6VtAuq_eYjPZtD16Nthxun2uq6IxX9UNUHwaNJnxmUk_n_zvMqHfzPTzRvjdt0weh_uWJ1rj4_6J_Poai95A7Ipu_uG9ScvLOWqcer83pjYCO85svkScc-DQ_9aWcVbGDyIZLpT5-LeXymTpfZ_FpnYOWzgBfEL3b69W1ZJrLQGHOlEBeA34o=s16-no" alt="Search"></img>
+          <img src="https://i.postimg.cc/3wszczTW/search.png" alt="Search"></img>
         </button>
         <div className="container p-0">
           
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export const Header = () => {
     <header className="navbar shadow-sm p-3 mb-5 bg-body rounded">
   <div className=" container">
       <img
-        src="https://lh3.googleusercontent.com/9L-iC0AIjn0nQJX2NKJ_DlGdud4Ll3amiA0icAHgnD3PRe0a6jqiiuAU5VYpb9ImTUGS2NwCTzt8kFDOZaKnahUjXg2zo7SgUakSNh0rXcIzluxOtBv4GTTggYn8y_ZZ3pU1-CisX9jT3jnmKo0Sf-_wpAgdfGnijfCeuw3XOs3nbPkF7gjP_yb4LJJqTyLEAKKCmW2GErJcXcJctABE3Ja4YoTS14YjUJJ_3jm-J9NfeuYeq9U23ZvLfaZMlOOcFOeImSXb8gd6RgggOLVsz7rJ9HlSDO2BO61brvvaiKgdnMapeAtTQwGij0oqJNvPHSc834s-OrR_lcCJrTUoXyKd1vUPTEXkeeCds9wnHOX718EKRecSxM1GLdcTZSKLybdjYOfiX1FoxwVM2vHNKUiz9Y-MN1ZDYfDZaUmS3Lm1YYkQ9p5f-GdGnQHl2ePAZedonfjLCkKN3Wt6qqcPV_Gp0Zpj7lEmgBdXyE24Sus-WzKEgLmGhBFu__b5xhHu3XyUDJ1LtS3FA6TQFUe1D19ekEZ3V5f-w9KB5N78kKr8bc6McXqDfyA45wbhBSaP4TggTwYzvIpippWxbyKKqE9xfPSXqbmaZaEU2awasOUcVWvWREd16xaMx_VDDEGo1Kr9wbQ4SLZ1T_TPgNHc6_pqwcuB-c3tER3Z8tpm2rdgWEhJatQV6lLyO8erE6Otab1YgFlhnU3seNWfWlJF_KkskuI2RWAg7VhwO9clgX2BZPE0ra_xFy6UCp0=w82-h50-no"
+        src="https://i.postimg.cc/tTF8QDqk/new-Houm-Logo.png"
         alt="HOUMLogo"
       />
   </div>
